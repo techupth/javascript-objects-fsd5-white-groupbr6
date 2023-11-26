a98c3e9175394453e59c20e37306de2b427acb32
Mae Okubo <okubo.mae@gmail.com>
2023-11-26 15:35:05 +0900

    Done Assignment Objects

--- ex-2.js
+++ ex-2.js
@@ -5,6 +5,15 @@ const product = {
   price: 4000,
   ratings: 2.5,
   photo: null,
+  code: 'IT0101010',
 };
-let socialMediaShare = "facebookShare";
+ product.socialMediaShare ={
+    facebookshare: 45.5,
+  };
 // Start code here
+product.ratings = 4.5;
+product.photo = 'https://i01.appmifile.com/webfile/globalimg/products/pc/mi-air-purifier-3H/replace_03.jpg';
+product.price = 6000;
+delete product.ratings;
+
+console.log(product );
